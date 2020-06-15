5b7efa55b47d4b135422a3658a0f3c27da3e3c83
Israel Abraham <israelvictory87@gmail.com>
2020-06-15 10:47:17 +0100

    Made migrations to the database.

--- anime/models.py
+++ anime/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Search(models.Model):
+    search = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.search
