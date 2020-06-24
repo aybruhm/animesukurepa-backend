92554dd38b12b8e1e110d7cdf7cc34c8f611281b
Israel Abraham <israelvictory87@gmail.com>
2020-06-24 20:06:05 +0100

    Added Bootstrap CDN. Modified the base and index template file.

--- anime/views.py
+++ anime/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Search
 
 
 def home(request):
     return render(request, 'base.html')
 
 def search(request):
-    return render(request, 'anime/index.html')
+    search = request.POST.get('title')
+    print(search)
+    context = {'search': search}
+    return render(request, 'anime/index.html', context)
