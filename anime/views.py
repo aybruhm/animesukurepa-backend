33533a26cdbbda51e6243228362fc6c6196e4ced
Israel Abraham <israelvictory87@gmail.com>
2020-08-10 10:24:07 +0100

    scraped the datas from https://www.gogoanime.movie/ and stored them on a list

--- anime/views.py
+++ anime/views.py
@@ -5,30 +5,41 @@ import requests
 from requests.compat import quote_plus
 
 
-
 AnimeUrl = 'https://www19.gogoanime.io//search.html?keyword={}'
+
 
 def home(request):
     return render(request, 'base.html')
 
+
 def search(request):
-    search = request.POST.get('title') # Gets whatever you search for
-    models.Search.objects.create(search=search) # Saves whatever you search for into the database
-    searchURL = AnimeUrl.format(quote_plus(search)) # Takes what you search for and inserts it in AnimeUrl(Original Url)
-    response = requests.get(searchURL) # this gets the whole URL address (e.g https://www19.gogoanime.io//search.html?keyword=whatever+you+searched+for)
-    data = response.text # prints the source code for debugging sake
+    # Gets whatever you search for
+    search = request.POST.get('title')
+
+    # Saves whatever you search for into the database
+    models.Search.objects.create(search=search)
+
+    # Takes what you search for and inserts it in AnimeUrl(Original Url)
+    searchURL = AnimeUrl.format(quote_plus(search))
+
+    # this gets the whole URL address (e.g https://www19.gogoanime.io//search.html?keyword=whatever+you+searched+for)
+    response = requests.get(searchURL)
+
+    data = response.text
     soup = BeautifulSoup(data, 'lxml')
-    
-    anime_listings = soup.find('ul', {'class':'items'})
 
-    for anime in anime_listings:
-        anime_title = anime.find('ul', {'class':'items'})
-        anime_url = anime.find('p', {'class': 'name'})
-        anime_images = anime.find('img').text
+    anime_listings = soup.find('div', class_='last_episodes')
 
-    
-        anime_listings.append((anime_title, anime_url, anime_images))
+    scraped_animes = []
 
+    for anime in anime_listings.find_all('li'):
 
-    context = {'search': search, 'anime_listings': anime_listings}
+        title = anime.find('p', class_='name').text
+        img_src = anime.find('img').get('src')
+        img_alt = anime.find('img').get('alt')
+        a_link = anime.find('a').get('href')
+
+        scraped_animes.append((title, img_src, img_alt, a_link))
+
+    context = {'search': search, 'scraped_animes': scraped_animes}
     return render(request, 'anime/index.html', context)
