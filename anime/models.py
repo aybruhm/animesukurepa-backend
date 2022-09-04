bd612872fc1eae8996e9c417d49793d8b5ec637e
Abram <israelvictory87@gmail.com>
2022-09-04 20:51:18 +0100

    Created Category and SavedAnime model, then made database migrations

--- anime/models.py
+++ anime/models.py
@@ -1,14 +1,41 @@
 from django.db import models
-from django.utils import timezone
 
 
 class Search(models.Model):
-    search = models.CharField(max_length=100)
-    timestamp = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return self.search
-
+    name = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name_plural = 'Searches'
+        verbose_name_plural = "Searched Anime"
+        db_table = "searches"
+        
+    def __str__(self) -> str:
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = "Anime Categories"
+        db_table = "anime_categories"
+        
+    def __str__(self) -> str:
+        return self.name
+
+
+class SavedAnime(models.Model):
+    title = models.CharField(max_length=255)
+    image_src = models.URLField()
+    image_alt = models.CharField(max_length=255)
+    anime_link = models.URLField()
+    released = models.CharField(max_length=100)
+    
+    class Meta:
+        verbose_name_plural = "Saved Animes"
+        db_table = "saved_animes"
+        
+    def __str__(self) -> str:
+        return self.title
+    
